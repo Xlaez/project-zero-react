0f48b6e1278868baa64ac4e3a7f1ab3c8c717878
Xlaez <utibeabasiekong2@gmail.com>
2022-04-29 05:44:35 +0100

    ready to deploy

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,2 +1,3 @@
-export const api = 'http://localhost:8081/';
+// export const api = 'http://localhost:8081/';
+export const api = 'https://intense-shelf-94291.herokuapp.com/'
 export const apiHost = `${api}api`;
